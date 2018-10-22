9685db22e0c0541ae499bf70c61323fd7b9087de
5donuts <charliegerman32@gmail.com>
2018-10-22 17:17:12 -0400

    update Destination class to also hold the _ID field for the DESTINATION_NODES table

--- app/src/main/java/edu/bridgewater/csci400/mapper/util/Destination.java
+++ app/src/main/java/edu/bridgewater/csci400/mapper/util/Destination.java
@@ -3,18 +3,23 @@ package edu.bridgewater.csci400.mapper.util;
 import java.util.List;
 
 public class Destination {
-    private int _id;
+    private int _id, nodes_id;
     private String name;
     private List<Node> nodes;
 
-    public Destination(int id, String name, List<Node> nodes) {
+    public Destination(int id, int nodes_id, String name, List<Node> nodes) {
         this._id = id;
+        this.nodes_id = nodes_id;
         this.name = name;
         this.nodes = nodes;
     }
 
     public int getId() {
         return _id;
+    }
+
+    public int getNodesId() {
+        return nodes_id;
     }
 
     public String getName() {

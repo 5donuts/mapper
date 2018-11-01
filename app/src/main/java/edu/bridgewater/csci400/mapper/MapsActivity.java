fcb7846b84892cf11b72f35f24175478aedba5c2
Istrali <istrali775@gmail.com>
2018-11-01 10:02:10 -0400

    Added demo code for parsing JSON

--- app/src/main/java/edu/bridgewater/csci400/mapper/MapsActivity.java
+++ app/src/main/java/edu/bridgewater/csci400/mapper/MapsActivity.java
@@ -9,6 +9,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.*;
+import android.util.Log;
+import java.io.*;
+import com.google.android.gms.maps.model.*;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -42,5 +46,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng bridgewaterCollege = new LatLng(38.3787678, -78.9705121);
         mMap.addMarker(new MarkerOptions().position(bridgewaterCollege).title("Bridgewater College"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bridgewaterCollege, 20)); // zoom level of 20
+
+        //Temporary demo code//
+        BufferedReader reader = null;
+        try {
+            InputStreamReader is = new InputStreamReader(getAssets().open("map_data.json"));
+            reader = new BufferedReader(is);
+
+            StringBuilder out = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+
+            String json = out.toString();
+
+            JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+            JsonArray nodes = jsonObject.getAsJsonObject("map_data").getAsJsonArray("nodes");
+            double[] coords = new double[500];
+            for (int i = 0; i < nodes.size(); i++) {
+                Log.d("json", nodes.get(i).getAsJsonObject().get("latitude").getAsString());
+                Circle circle = mMap.addCircle(new CircleOptions()
+                        .center(new LatLng(nodes.get(i).getAsJsonObject().get("latitude").getAsDouble(), nodes.get(i).getAsJsonObject().get("longitude").getAsDouble()))
+                        .radius(2)
+                        .strokeWidth(7)
+                        .strokeColor(0xFF49C7FF)
+                        .fillColor(0xFFFFFFFF));
+                coords[2*i] = nodes.get(i).getAsJsonObject().get("latitude").getAsDouble();
+                coords[(2*i)+1] = nodes.get(i).getAsJsonObject().get("longitude").getAsDouble();
+            }
+            JsonArray edges = jsonObject.getAsJsonObject("map_data").getAsJsonArray("edges");
+            for (int i = 0; i < edges.size(); i++) {
+                Log.d("json", edges.get(i).getAsJsonObject().get("node1").getAsString());
+                int node1 = edges.get(i).getAsJsonObject().get("node1").getAsInt();
+                int node2 = edges.get(i).getAsJsonObject().get("node2").getAsInt();
+                Polyline path = mMap.addPolyline(new PolylineOptions()
+                        .add(new LatLng(coords[(node1-1)*2], coords[(node1-1)*2+1]), new LatLng(coords[(node2-1)*2], coords[(node2-1)*2+1]))
+                        .width(5)
+                        .color(0xFF49C7FF));
+            }
+        } catch (IOException e) {
+            //log the exception
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+        //--------------------//
     }
 }

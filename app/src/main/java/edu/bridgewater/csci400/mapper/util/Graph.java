ce94c8a4c38d4901460f0947a23a73f1be032543
5donuts <charliegerman32@gmail.com>
2018-11-13 18:30:50 -0500

    Closes #24 List<Polyline> getPaths() was changed to List<Polyline> getPaths(GoogleMap) and implemented.

--- app/src/main/java/edu/bridgewater/csci400/mapper/util/Graph.java
+++ app/src/main/java/edu/bridgewater/csci400/mapper/util/Graph.java
@@ -1,13 +1,21 @@
 package edu.bridgewater.csci400.mapper.util;
 
+import android.graphics.Color;
+
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.bridgewater.csci400.mapper.db.LocalDB;
 
 public class Graph {
+    private static final float POLYLINE_WIDTH = 5.0F;
+    private static final int POLYLINE_COLOR = Color.BLUE;
+    private static final int SELECTED_POLYLINE_COLOR = Color.RED;
     private List<Node> nodes;
     private List<Edge> edges;
     private List<Destination> destinations;
@@ -18,8 +26,18 @@ public class Graph {
         //TODO implement getDestinations()
     }
 
-    public List<Polyline> getPaths(){
-        return null;
+    public List<Polyline> getPaths(GoogleMap map){
+        List<Polyline> polylines = new ArrayList<>();
+        for(Edge e : edges) {
+            List<Node> nodes = e.getNodes();
+            Polyline p = map.addPolyline(new PolylineOptions()
+                    .add(nodes.get(0).getPosition(), nodes.get(1).getPosition())
+                    .width(POLYLINE_WIDTH)
+                    .color(POLYLINE_COLOR)
+            );
+            polylines.add(p);
+        }
+        return polylines;
     }
 
     public List<LatLng> getDestinationPins() {

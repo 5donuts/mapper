09dfb3df9f229159ebcc81f9211ac61b56ad3a5d
5donuts <5donuts@users.noreply.github.com>
2018-12-12 14:23:47 -0500

    update Node distance method

--- app/src/main/java/edu/bridgewater/csci400/mapper/util/Node.java
+++ app/src/main/java/edu/bridgewater/csci400/mapper/util/Node.java
@@ -43,10 +43,20 @@ public class Node {
     }
 
     public double distanceTo(Node n) {
-        LatLng pos1 = position;
-        LatLng pos2 = n.getPosition();
+        LatLng p1 = getPosition();
+        LatLng p2 = n.getPosition();
+        final double R =  6378137; // Earth’s mean radius in meters
+        double dLat = deg2rad(p2.latitude - p1.latitude);
+        double dLong = deg2rad(p2.longitude - p1.longitude);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(deg2rad(p1.latitude)) * Math.cos(deg2rad(p2.latitude)) *
+                        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c; // Distance in meters
+        return d;
+    }
 
-        return Math.sqrt(Math.pow(pos2.latitude - pos1.latitude, 2)
-                + Math.pow(pos2.longitude - pos1.longitude, 2));
+    private double deg2rad(double x) {
+        return x * Math.PI / 180;
     }
 }
